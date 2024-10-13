666e838502dbf769f48fa085ecb0e0579a61c5c5
Nick Anthony Miras <mirasnickanthony@gmail.com>
2024-10-13 17:26:55 +0800

    added nasa .nc4 data from GPM Archives

---
+++ nasa_data/README.md
@@ -0,0 +1,5 @@
+# NASA DATA
+
+This repository contains the data from the NASA GPM (Global Precipitation Measurement) Archive. 
+The data is stored in the `precipitation_data` directory. The data is in netcdf format. 
+This contains data from November 2023 until April 2024.
---
+++ notebooks/download_nc_files.py
@@ -0,0 +1,40 @@
+import re
+import requests
+import os
+
+
+NC_FILE_LIST = 'nasa_data/subset_GPM_3IMERGDF_07_20241013_074527_.txt'
+DOWNLOAD_DIR = 'nasa_data/precipitation_data'
+
+with open (NC_FILE_LIST, 'r') as f:
+    nc_files = f.readlines()
+
+nc_files = [f.strip() for f in nc_files[2:]]
+
+for file_link in nc_files:
+    # get the date from the file name using regex
+    date = re.search(r'\d{8}', file_link).group()
+    year = date[0:4]
+    month = date[4:6]
+    day = date[6:8]
+    file_name = f'{year}-{month}-{day}.nc4'
+
+    with requests.Session() as s:
+        s.auth = (os.getenv('NASA_USER_NAME'), os.getenv('NASA_PASSWORD'))
+        
+        try:
+            response = s.get(s.request('get', file_link).url, stream=True)
+            file_path = os.path.join(DOWNLOAD_DIR, file_name)
+            if response.status_code == 200:
+                with open(file_path, 'wb') as f:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        f.write(chunk)
+                print(f"Downloaded {file_name}")
+            else:
+                print(f"Failed to download {file_name}: {response.status_code}")
+                with open("failed_downloads.log", "a") as log_file:
+                    log_file.write(f"{file_link}\n")
+        except Exception as e:
+            print(f"Error downloading {file_name}: {e}")
+            with open("failed_downloads.log", "a") as log_file:
+                log_file.write(f"{file_link}\n")
