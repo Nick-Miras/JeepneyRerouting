0b0ef0d38d91035e8150715f5e0c341744f1056c
Nick Anthony Miras <mirasnickanthony@gmail.com>
2024-11-14 22:59:32 +0000

    added scripts

--- scripts/precipitation.py
+++ scripts/precipitation.py
@@ -2,27 +2,18 @@ import ecmwf.data as ecdata
 from ecmwf.opendata import Client
 import metview as mv
 
-client = Client("ecmwf")
 
-# total precipitation probability
-parameters = ['tpg1', 'tpg5' ,'tpg10' ,'tpg20' ,'tpg25' ,'tpg50' ,'tpg100']
+parameters = ['tp']
 filename = 'data/precipitation/medium-tp-mean.grib2'
 
-client.retrieve(
-    step="24-48",  # get the forecast for tomorrow
-    stream="enfo",
-    type="ep",
-    levtype="sfc",
-    param=parameters,
-    target=filename
-)
 
-data = mv.read(filename)
-mv.grib_get(data, ['shortName', 'level', 'step'])
+def get_fieldset():
+    data = mv.read(filename)
+    return data
 
 
-#def interpolate_precipitation(fieldset, df):
-#    df = df.copy()
-#    interpolated_data = mv.interpolate(fieldset, data['lat'], data['lon'])
-#    df['precipitation'] = interpolated_data
-#    return df
+def interpolate_precipitation(fieldset, df):
+    df = df.copy()
+    interpolated_data = mv.interpolate(fieldset, df['lat'], df['lon'])
+    df['precipitation'] = interpolated_data
+    return df
--- scripts/rerouting.py
+++ scripts/rerouting.py
@@ -1,23 +1,87 @@
-
-def readlines_in_file(directory_path):
-    # Initialize an empty string to store the concatenated content
-    concatenated_content = []
-    # Loop through all files in the directory
-    for filename in os.listdir(directory_path):
-        # Check if the file has a '.txt' extension
-        if filename.endswith('.graphml') and filename.startswith('2020') is False and filename.startswith('mask') is False:
-            # Get the full file path
-            file_path = os.path.join(directory_path, filename)
-            # Open the text file and read its content
-            with open(file_path, 'r', encoding='utf-8') as file:
-                # concatenated_content += file.read() + "\n"  # Adding newline to separate files' content
-                for line in file.readlines():
-                    concatenated_content.append(line.strip())
-
-    return concatenated_content
+import matplotlib.pyplot as plt
+import contextily as cx
+import networkx as nx
+import pandas as pd
+import osmnx as ox
+import random
+import json
+import geopandas as gpd
+import numpy as np
+import random
+import itertools
 
 
+import overpass
+from shapely.geometry import Polygon, LineString, Point
+import overpy
+from osmapi import OsmApi
 
-jeepney_routes = readlines_in_file('data/routes/relations.txt')
+
+BOUNDING_BOX = (14.64106, 14.54603, 121.12186, 120.95226)
+graph_of_area = ox.graph_from_bbox(bbox=BOUNDING_BOX)
+
+subgraph = ox.io.load_graphml('../graphs/graphml/jeepneyroutes/T307-graph-original.graphml')
 
 
+def get_nearest_node_and_delete_it(node_id, graph, x, y):
+    graph.remove_node(node_id)
+    return get_nearest_node(graph, x, y)
+
+
+def get_nearest_node(graph, x, y):
+    nn, _ = tuple(nn_dist for nn_dist in ox.distance.nearest_nodes(graph.copy(), x, y, return_dist=True))
+    return nn
+
+
+def get_random_lst_of_nodes(graph, num_nodes_to_delete):
+    random_nodes = []
+    counter = 0
+    while True:
+        if counter == num_nodes_to_delete:
+            break
+        node = random.choice(list(graph.nodes))
+        if len(list(graph.neighbors(node))) < 2:
+            continue
+        else:
+            random_nodes.append(node)
+            counter += 1
+    return random_nodes
+
+
+def get_lst_of_rerouted_paths(G, G_sub, deleted_nodes):
+    shortest_paths_between_closest_nodes = []
+    for node_id in deleted_nodes:
+        try:
+            if len(neighbors := list(G_sub.neighbors(node_id))) < 2:
+                continue
+        except nx.NetworkXError:
+            continue
+        else:
+            G.remove_node(node_id)
+            G_sub.remove_node(node_id)
+            try:
+                shortest_path = nx.shortest_path(G, source=neighbors[0], target=neighbors[1])
+                print(shortest_path)
+                shortest_paths_between_closest_nodes.append(shortest_path)
+                existing_nodes = set(shortest_path).intersection(G_sub.nodes)
+                g = nx.articulation_points(nx.MultiGraph(nx.compose(graph_of_area.subgraph(shortest_path), G_sub)))
+                exclude = existing_nodes.intersection(g)
+                print(exclude)
+                if len(existing_nodes) > 2:
+                    G_sub.remove_nodes_from(list(existing_nodes - exclude))
+            except nx.NetworkXNoPath:
+                continue
+    return shortest_paths_between_closest_nodes
+
+
+def get_reroute(G_sub, shortest_paths_between_closest_nodes):
+    rerouted_sub_graph_lst = []
+    for path in shortest_paths_between_closest_nodes:
+        rerouted_sub_graph_lst.append(graph_of_area.subgraph(path))
+    return nx.compose(G_sub, nx.compose_all(rerouted_sub_graph_lst))
+
+# G = graph_of_area.copy()
+# G_sub = subgraph.copy()
+# num_nodes_to_delete = 5
+# deleted_nodes = get_random_lst_of_nodes(G_sub, num_nodes_to_delete)
+# print(deleted_nodes)
